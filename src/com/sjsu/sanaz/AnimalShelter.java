99e95b3d00daeb555e3d24b01afe5e62c4bde43e
sanazkh <sanaz.khosravi70@gmail.com>
2018-09-12 23:45:25 -0700

    new questions answers

---
+++ src/com/sjsu/sanaz/AnimalShelter.java
@@ -0,0 +1,7 @@
+package com.sjsu.sanaz;
+
+/**
+ * Created by sanazk on 9/12/18.
+ */
+public class AnimalShelter {
+}
---
+++ src/com/sjsu/sanaz/ThreeStacksInOneArray.java
@@ -0,0 +1,7 @@
+package com.sjsu.sanaz;
+
+/**
+ * Created by sanazk on 9/9/18.
+ */
+public class ThreeStacksInOneArray {
+}
